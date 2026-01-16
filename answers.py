714a648c0b6d2e118954d4c8776f658a2813dca3
sohum-mehrotra <axy3hu@virginia.edu>
2026-01-16 20:50:05 +0000

    added answers

---
+++ answers.py
@@ -0,0 +1,15 @@
+# %%
+
+# What is your terminal display "path"? (type/paste as text into the .py file):
+# @sohum-mehrotra ➜ /workspaces/daytwostuff-SM (main) $ 
+
+# Should you include the environment in your repo or not?
+# yes
+
+# Now what is your terminal display "path"? Is it different?
+# (venv) @sohum-mehrotra ➜ /workspaces/daytwostuff-SM (main) $ 
+
+import pandas as pd
+
+# raw link
+data = pd.read_csv('https://raw.githubusercontent.com/MMiDS-textbook/MMiDS-textbook.github.io/refs/heads/main/utils/datasets/penguins-measurements.csv')
